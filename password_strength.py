d77790da6c8730c7e9c7c1edc72a64d2ad2f94ec
OnlineAlex <becasualua@gmail.com>
2018-03-20 15:51:10 +0200

    Убрал bool() де было возможно.  if len(password) < len_normal_password - этот оставил, что бы функция продолжала работать, если пароль достаточно длинный.
    Переименовал функции более грамотно.
    split('\n') -> splitlines()

--- password_strength.py
+++ password_strength.py
@@ -3,22 +3,22 @@ import getpass
 
 
 def is_password_long(password):
-    len_good_psw = 8
-    return bool(len(password) >= len_good_psw)
+    len_good_password = 8
+    return len(password) >= len_good_password
 
 
-def in_password_char_and_num(password):
+def has_char_and_num(password):
     return bool(re.search(
         '[a-zA-Z].*\d|\d.*[a-zA-Z]',
         password
     ))
 
 
-def in_password_special_char(password):
+def has_special_char(password):
     return bool(re.search('\W+', password))
 
 
-def are_char_different_register(password):
+def has_char_different_register(password):
     return bool(re.search(
         '[A-Z].*[a-z]|[a-z].*[A-Z]',
         password
@@ -32,10 +32,10 @@ def load_data_list(filepath):
 
 
 def is_in_blacklist(password, blacklist):
-    return bool(password in blacklist)
+    return password in blacklist
 
 
-def is_in_names_list(password, name_list):
+def has_name_in_password(password, name_list):
     for popular_name in name_list:
         if popular_name in password.lower():
             return True
@@ -43,7 +43,7 @@ def is_in_names_list(password, name_list):
     return False
 
 
-def evaluation_uniqueness(in_blacklist, name_include):
+def get_rating_uniqueness(in_blacklist, name_include):
     if not in_blacklist and not name_include:
         return 3
     elif not in_blacklist:
@@ -58,25 +58,25 @@ def are_dates_include(password):
     return not bool(re.search('\d{4,}', password))
 
 
-def are_duplicate_char(password):
+def has_duplicate_char(password):
     return not bool(re.search('(\S)\1\1', password))
 
 
-def get_password_strength(password, rating_uniq):
-    len_norm_psw = 5
-    if len(password) < len_norm_psw:
+def get_password_strength(password, rating_uniqueness):
+    len_normal_password = 5
+    if len(password) < len_normal_password:
         return 1
 
     testing_list = [
         is_password_long,
-        in_password_char_and_num,
-        in_password_special_char,
-        are_char_different_register,
+        has_char_and_num,
+        has_special_char,
+        has_char_different_register,
         are_dates_include,
-        are_duplicate_char
+        has_duplicate_char
     ]
 
-    strength_rating = 1 + rating_uniq
+    strength_rating = 1 + rating_uniqueness
     for test in testing_list:
         if test(password):
             strength_rating += 1
@@ -89,11 +89,13 @@ if __name__ == '__main__':
     if user_password:
         blacklist_passwords = load_data_list('10-million-password-list-top-100000.txt')
         blacklist_names = load_data_list('names.txt')
-        rating_uniqueness = evaluation_uniqueness(
+        rating_uniqueness_user_password = get_rating_uniqueness(
             is_in_blacklist(user_password, blacklist_passwords),
-            is_in_names_list(user_password, blacklist_names)
+            has_name_in_password(user_password, blacklist_names)
         )
-        password_strength = get_password_strength(user_password, rating_uniqueness)
+        password_strength = get_password_strength(
+            user_password,
+            rating_uniqueness_user_password)
 
         print('Надежность пароля {}'.format(password_strength))
     else:
